de1fed03d5ad112269032b316e411ef3109e332a
Raees <muhammadraeesahmed38@gmail.com>
2022-09-04 00:26:01 +0500

    complete useState mini project

--- src/Components/Miniusestate.jsx
+++ src/Components/Miniusestate.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 
 const design = {
@@ -8,15 +9,32 @@ const design = {
 
 }
 function Miniusestate() {
+
+const [text,setText]=useState("Enter Text Here");
+
+   const  changetexttoUC=(()=>{
+console.log("clicked")
+setText(text.toUpperCase());
+    });
+
+   const  changetexttoLC=(()=>{
+    console.log("clicked")
+    setText(text.toLowerCase());
+    })
+   
+    const changehandler=((event)=>{
+setText(event.target.value);
+    })
+
     return (
-        <div className="container bg-light"  style={{marginTop:'180px'}}>
+        <div className="container bg-light" style={{ marginTop: '180px' }}>
             <div className="mb-3">
-            
-               <textarea className="form-control"  id="myBox" rows={8}>Enter some text</textarea>
+
+                <textarea className="form-control" id="myBox" rows={8}  value={text} onChange={changehandler}> </textarea>
             </div>
-            <button className='btn btn-primary'>Convert to Upercase</button>
-            
-            <button className='btn btn-primary' style={{marginTop:'30px'}}>Convert to Lowercase</button>
+            <button className='btn btn-primary'  onClick={changetexttoUC}>Convert to Upercase</button>
+
+            <button className='btn btn-primary' style={{ marginTop: '30px' }} onClick={changetexttoLC}>Convert to Lowercase</button>
         </div>
     )
 }
