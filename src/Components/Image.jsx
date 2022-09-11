1484b0f53ec6262985b9706c28d616be5fb02872
Raees <muhammadraeesahmed38@gmail.com>
2022-09-11 13:02:30 +0500

    ui done

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 
+
 const imgstyle={
     display:' block',
     marginLeft: 'auto',
@@ -8,12 +9,45 @@ const imgstyle={
     marginTop:'30px',
     width: '50%;'
 }
+
+const row={
+    textAlign:'center',
+/*the same margin which is every button have, it is for small screen, and if you have many buttons.*/
+marginLeft:'-20px',
+marginRight:'-20px',
+marginTop:'30px'
+}
+
+const appbutton={
+    backgroundColor: '#01AAAD',
+    width: '150px',
+    margin: '20px',
+    display:'inline-block',
+    lineHeight: '60px'
+}
 function Image() {
 
     
   return (
     <div >
         <img src="https://picsum.photos/250/300" alt="Girl in a jacket"  style={imgstyle}></img>
+
+        <div style={row}>
+  <input
+    type="submit"
+   
+    style={appbutton}
+    id="btnSearch"
+    defaultValue="Aply Filters"
+  />
+  <input
+    type="submit"
+   
+    style={appbutton}
+    id="btnClearSearch"
+    defaultValue="Clear Filters"
+  />
+</div>
     </div>
   )
 }
