7910e2b6c5f2df889049c5781ef42816603aa8ec
Raees <muhammadraeesahmed38@gmail.com>
2022-09-12 23:09:57 +0500

    done miniRandom useState project

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,55 +1,55 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
+const imgstyle = {
+  display: " block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "30px",
+  width: "50%;",
+};
 
-
-const imgstyle={
-    display:' block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop:'30px',
-    width: '50%;'
+const row = {
+  textAlign: "center",
+  /*the same margin which is every button have, it is for small screen, and if you have many buttons.*/
+  marginLeft: "-20px",
+  marginRight: "-20px",
+  marginTop: "30px",
 }
 
-const row={
-    textAlign:'center',
-/*the same margin which is every button have, it is for small screen, and if you have many buttons.*/
-marginLeft:'-20px',
-marginRight:'-20px',
-marginTop:'30px'
-}
-
-const appbutton={
-    backgroundColor: '#01AAAD',
-    width: '150px',
-    margin: '20px',
-    display:'inline-block',
-    lineHeight: '60px'
+const appbutton = {
+  backgroundColor: "#01AAAD",
+  width: "150px",
+  margin: "20px",
+  display: "inline-block",
+  lineHeight: "60px",
 }
 function Image() {
+  let url="https://picsum.photos/250/300";
+  const [image, setImage] = useState(url);
 
-    
+  const btnNext=()=>{
+    console.log("Clicked Next Button");
+    setImage("https://picsum.photos/200");
+  }
+  const btnBack=()=>{
+    console.log("Clicked Back Button");
+    setImage("https://picsum.photos/seed/picsum/200/300");
+  }
   return (
-    <div >
-        <img src="https://picsum.photos/250/300" alt="Girl in a jacket"  style={imgstyle}></img>
+    <div>
+      <img src={image} alt="Girl in a jacket" style={imgstyle}></img>
 
-        <div style={row}>
-  <input
-    type="submit"
-   
-    style={appbutton}
-    id="btnSearch"
-    defaultValue="Aply Filters"
-  />
-  <input
-    type="submit"
-   
-    style={appbutton}
-    id="btnClearSearch"
-    defaultValue="Clear Filters"
-  />
-</div>
+      <div style={row}>
+        <button type="button" style={appbutton} onClick={btnBack}>
+          Back!
+        </button>
+        <button type="button" style={appbutton} onClick={btnNext}>
+          Next!
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Image
+export default Image;
